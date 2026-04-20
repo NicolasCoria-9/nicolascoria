eb3b007efc82179d9ebb6c6eccb9490f8f9af394
Nicolas_Coria <nicolasacoria@gmail.com>
2026-04-20 20:24:09 -0300

    c

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
-    adapter: github(),
     build: {
         format: 'directory'
     }
