b060342de44e55985f1c1c1ed440390932ec37ed
Nicolas_Coria <nicolasacoria@gmail.com>
2026-04-21 21:55:04 -0300

    ver

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://nicolascoria-9.github.io',
-  base: '/nicolascoria',
+    output: 'static',
+    site: 'https://nicolascoria-9.github.io/nicolascoria/',
+    base: '/'
 });
